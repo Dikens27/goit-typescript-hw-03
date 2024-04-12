4f7430a0ff013b79a9c07cbd130de8d82368fd9f
Dikens27 <kar.karovich321@gmail.com>
2024-04-12 21:16:52 +0300

    homework

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,53 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  public getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door: boolean = false;
+  public tenants: Person[] = [];
+
+  constructor(public key: Key) {}
+
+  public comeIn(tenant: Person): void {
+    if (this.door) {
+      this.tenants.push(tenant);
+      console.log(this.tenants);
+    } else {
+      alert("The door is closed");
+    }
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +56,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
